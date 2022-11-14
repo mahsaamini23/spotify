5f96e358ae5d3f73340df143fed6cc6f94bf09e1
ms.moradi23 <ms.moradi23@yahoo.com>
2022-11-14 15:46:10 +0330

    add drawer and appBar and styles

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,81 @@
 import styles from '../styles/Home.module.css'
+import Grid from '@mui/material/Grid';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Box from '@mui/material/Box';
+import Image from 'next/image';
+import logo from '../image/spotify-logo.png';
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import Typography from '@mui/material/Typography';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import IconButton from '@mui/material/IconButton';
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      Next.js
-      <h1>Hello Next</h1>
-    </div>
+    <Grid>
+      <Grid >
+        <AppBar sx={{backgroundColor:'#101010', }}>
+          <Toolbar sx={{marginLeft:'230px',display:'flex', justifyContent:'space-between'}}>
+          <Grid>
+              <IconButton>
+                <ArrowBackIosIcon sx={{color:'#b2b2b2',fontSize:20}}/>
+              </IconButton>
+              <IconButton>
+                <ArrowForwardIosIcon sx={{color:'#b2b2b2',fontSize:20}}/>
+              </IconButton>
+            </Grid>
+            <Grid sx={{display:'flex', gap:'15px'}}>
+              <Button sx={{color:'#b2b2b2',fontSize:'12px', fontWeight:'bolder'}}>Sign up</Button>
+              <Button sx={{backgroundColor:"white", color:'#000',borderRadius:'30px', padding:"12px 28px",fontSize:'12px', fontWeight:'bolder'}} >Log in</Button>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      </Grid>
+      <Drawer variant="permanent" PaperProps={{sx: { width: "18%" ,backgroundColor:'#000'},}}>
+        <Box sx={{backgroundColor:'#000', padding:'22px 0 10px 22px'}}>
+          <Image 
+            src={logo}
+            width={120}
+            height={36}
+          />
+        </Box>
+        <List>
+          <Box sx={{marginBottom:'30px'}}>
+            <ListItem sx={{display:'flex', gap:'10px',height:'35px'}}>
+              <HomeIcon sx={{color:'#fff',fontSize:30}}/>
+              <Typography sx={{color:'#fff', fontSize:'12px', fontWeight:'bolder'}}>Home</Typography>
+            </ListItem>
+            <ListItem sx={{display:'flex', gap:'10px',height:'35px'}}>
+              <SearchIcon sx={{color:'#b2b2b2',fontSize:30}}/>
+              <Typography sx={{color:'#b2b2b2',fontSize:'12px', fontWeight:'bolder'}}>Search</Typography>
+            </ListItem>
+            <ListItem sx={{display:'flex', gap:'10px',height:'35px'}}>
+              <VideoLibraryIcon sx={{color:'#b2b2b2'}}/>
+              <Typography sx={{color:'#b2b2b2',fontSize:'12px', fontWeight:'bolder'}}>Your Library</Typography>
+            </ListItem>
+          </Box>
+          <Box>
+          <ListItem sx={{display:'flex', gap:'10px',height:'35px'}}>
+              <AddBoxIcon sx={{color:'#b2b2b2',fontSize:30}}/>
+              <Typography sx={{color:'#b2b2b2',fontSize:'12px', fontWeight:'bolder'}}>Create Playlist</Typography>
+            </ListItem>
+            <ListItem sx={{display:'flex', gap:'10px',height:'35px'}}>
+              <BookmarkAddIcon sx={{color:'#b2b2b2',fontSize:30}}/>
+              <Typography sx={{color:'#b2b2b2',fontSize:'12px', fontWeight:'bolder'}}>Liked Songs</Typography>
+            </ListItem>
+          </Box>
+        </List>
+      </Drawer>
+    </Grid>
   )
 }
